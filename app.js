a69f7812a66331a9f5ab22629704cdb2addb0be7
Knpranav <darklight@gmail.com>
2022-01-17 09:10:01 +0530

    ok

---
+++ app.js
@@ -0,0 +1,42 @@
+
+var weather = {
+    'apiKey':'ed62e0360339103127f54550ed173945',
+    weatherFetch: function (city) {
+     
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&units=metric&appid="+this.apiKey)
+        .then((res)=>res.json())
+        //  .then((data)=>console.log(data))
+          .then((data)=> {this.displayWeather(data)
+            console.log(data);
+          });  
+    },
+
+    displayWeather:function(data){
+        const{name}=data;
+        const{icon,description}=data.weather[0];
+        const{temp,humidity,temp_max,temp_min}=data.main;
+        const{speed}=data.wind;
+        document.querySelector('.city').innerHTML= name.toUpperCase()
+        document.querySelector('.icon').src='https://openweathermap.org/img/wn/'+ icon + '.png'
+        document.querySelector('.temp').innerHTML= temp+'°C'
+        document.querySelector('.description').innerHTML= description
+        document.querySelector('.humidity').innerHTML = 'Humidity : '+humidity+'%'
+        document.querySelector('.max').innerHTML = ' Max : '+temp_max+'°C';
+        document.querySelector('.min').innerHTML = ' Min : '+temp_min+'°C';
+        document.querySelector('.wind').innerHTML = 'Wind Speed : '+ speed+'kmph'
+        document.querySelector('.search-bar').value=''
+    
+    },
+    search:function(){
+            var val = document.querySelector('.search-bar').value
+            this.weatherFetch(val)
+       
+    }
+};
+document.querySelector('.svg').addEventListener("click",function (){
+    weather.search();
+});
+document.querySelector('.search-bar').addEventListener('keyup',function(event){
+    if(event.key=='Enter')
+    weather.search()
+})
---
+++ login.js
@@ -0,0 +1,102 @@
+function jumpNext(event,autoMove){
+    var x = event.which
+    if(x==13){
+         document.getElementById(autoMove).focus();
+    }
+}
+
+function enter(event){
+    var key = event.which
+    if(key==13)
+    window.location.href = 'index1.html'
+}
+
+function edit(){
+    let ele = document.getElementById('enter')
+    ele.style.backgroundColor = 'rgb(83, 201, 142)'
+    ele.style.outline = 'none'
+    ele.style.transform = 'translateY(-5px)'
+
+}
+
+const togglePassword = document.querySelector('#togglePassword');
+const togglePassword1 = document.querySelector('#togglePassword1');
+var password = document.querySelector('#pass');
+var password1 = document.querySelector('#pass1');
+
+togglePassword.addEventListener('click', function (e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    pass.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+});
+togglePassword1.addEventListener('click', function (f) {
+    const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    pass1.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+});
+
+
+function check(){
+    var Uname = 'Uname'
+    var Pass = 'Pass'
+    
+  
+      
+          var usernameRegex= /\w{8,15}/
+          var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+          var passwordRegex= /^(?=.*[0-9])(?=.*[!@#$%&*])[a-zA-Z0-9!@#$%&*]{7,15}$/
+          var phonenumberRegex = /^\d{10}$/
+       var username = document.getElementById('username').value
+       var email = document.getElementById('email').value
+       var password = document.getElementById('pass').value
+       var phone = document.getElementById('phoneNumber').value
+  
+       localStorage.setItem(Uname,username)
+       localStorage.setItem(Pass,password)
+            
+    if(usernameRegex.test(username)==true)
+    { 
+        if(emailRegex.test(email)==true)
+        { 
+            
+          if(passwordRegex.test(password)==true)           {
+             if(phonenumberRegex.test(phone)==true)
+                 {  
+                   alert('Success!!')
+                   window.location.replace("./index.html")
+                 }    
+
+            else
+            alert('invalid phone-number')
+           
+          }
+          else
+          alert('Invalid password')
+         
+        }
+        else
+        alert('Invalid email')
+       
+    }
+    else
+    alert('Invalid Username')
+}
+
+
+function login(){
+    var Uname = 'Uname'
+    var Pass = 'Pass'
+    let inp_username = document.getElementById('username').value
+    let inp_pass = document.getElementById('pass').value
+    let storedUname = localStorage.getItem(Uname)
+    let storedPass = localStorage.getItem(Pass)
+    // if(inp_username == storedUname && inp_pass == storedPass)
+    if(1==0)
+        {
+          alert('success!!')
+          window.location.replace("./index2.html")
+      }
+  }
+  
+
+
